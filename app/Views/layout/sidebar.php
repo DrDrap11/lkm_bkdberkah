f395826b0ae3773f1f69e97a9df27efec7d05d45
Dimas Rizqi Agung Pratama <kmu856@gmail.com>
2023-03-14 20:42:20 +0700

    awal

---
+++ app/Views/layout/sidebar.php
@@ -0,0 +1,18 @@
+<li class="menu-header">Dashboard</li>
+<li class="active"><a class="nav-link" href="blank.html"><i class="fas fa-fire"></i> <span>Dashoard</span></a></li>
+              <li class="menu-header">Data Induk</li>
+              <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Data Karyawan</span></a>
+                <ul class="dropdown-menu">
+                  <li><a class="nav-link" href="layout-default.html">Daftar Jabatan</a></li>
+                  <li><a class="nav-link" href="layout-transparent.html">Data Karyawan</a></li>
+                </ul>
+<li class="active"><a class="nav-link" href="blank.html"><i class="fas fa-fire"></i> <span>Data Nasabah</span></a></li>
+              </li>
+              <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-th"></i> <span>Bootstrap</span></a>
+                <ul class="dropdown-menu">
+                  <li><a class="nav-link" href="bootstrap-alert.html">Alert</a></li>
+                </ul>
+              </li>
+              
